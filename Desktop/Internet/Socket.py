18ab900f27dc231d77f810610a7f0ce115bfed45
filecloud1234 <110065019+filecloud1234@users.noreply.github.com>
2024-02-07 03:04:40 -0800

    Update Socket.py

--- Desktop/Internet/Socket.py
+++ Desktop/Internet/Socket.py
@@ -1,1 +1,13 @@
 
+#pip install websocket-client
+from websocket import create_connection
+
+ws = create_connection("ws://192.168.10.2:80/connect-websocket")
+print(ws.recv())
+print("Sending 'Hello, World'...")
+ws.send("Hello, World")
+print("Sent")
+print("Receiving...")
+result =  ws.recv()
+print("Received '%s'" % result)
+ws.close()
